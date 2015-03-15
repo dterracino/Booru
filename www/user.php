6f4338aa2c7fbaf295d2ecf0d6042d7518c3f90d
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-15 20:37:03 +0100

    Improved user page

--- www/user.php
+++ www/user.php
@@ -19,12 +19,19 @@ if (isset($_GET["id"]))
 			$user = $result->fetch_assoc();
 			html_header("Booru - " . $user["username"]);
 
-			echo '<span style="font-size: 40px">' . $user["username"];
-			echo '</span><br><img alt="" src="avatar.php?id=' . $id . '">';
+			echo '<img alt="" src="avatar.php?id=' . $id . '"><br><br>';
+			echo '<span style="font-size: 40px">' . $user["username"] . "</span>";
+			if ($user["job"] != "")
+				echo "<br>" . $user["job"];
+
 			echo "<br><br><b>Permissions</b>";
 			foreach ($user as $key => $value)
 				if (substr($key, 0, 2) == "p_" && $value == 1)
 					echo "<br>" . $key;
+
+			echo "<br><br><b>Tools</b>";
+			echo '<a href="posts.php?tags=user%3D' . $user["username"] . '">';
+			echo "Search for posts uploaded by " . $user["username"] . "</a>";
 
 			html_footer();
 		}
