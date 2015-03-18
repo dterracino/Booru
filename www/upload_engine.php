8dec0dce89634efa6f3d856f37a77a251d691e18
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-18 12:29:09 +0100

    Implemented upload engine

---
+++ www/upload_engine.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once("db.php");
+require_once("config.php");
+require_once("session.php");
+
+function check_image($image_data, $mime)
+{
+	return $image_data;
+}
+
+function get_tag_id($tag)
+{
+	global $db;
+	$stmt = $db->prepare("SELECT id FROM tags WHERE tag = ?");
+	$stmt->bind_param("s", $tag);
+	if (!$stmt->execute())
+		throw new Exception("Couldn't select tag");
+	$result = $stmt->get_result();
+	if ($result->num_rows < 1)
+	{
+		$stmt = $db->prepare("INSERT INTO tags (tag) VALUES (?)");
+		$stmt->bind_param("s", $tag);
+		if (!$stmt->execute())
+			throw new Exception("Couldn't add tag");
+		return $db->insert_id;
+	}
+	else return $result->fetch_row()[0];
+}
+
+function upload_engine($image_data, $private, $source, $info, $rating, $tags)
+{
+	if (!session_loggedin())
+		return "Not logged in";
+
+	if (!session_has_perm("upload"))
+		return "No permission";
+
+	global $db, $thumb_dir, $image_dir, $mime_types;
+
+	$finfo = finfo_open();
+	$mime = finfo_buffer($finfo, FILEINFO_MIME_TYPE);
+	finfo_close($finfo);
+	if (!array_key_exists($mime, $mime_types))
+		return "MIME Type not allowed";
+
+	$image_data = check_image($image_data, $mime);
+	if ($image_data === FALSE)
+		return "Image file corrupt";
+
+	$size = getimagesizefromstring($image_data);
+	$width = $size[0];
+	$height = $size[1];
+	return "Couldn't determine image size";
+
+	$uid = session_user_id();
+	$hash = substr(hash("sha256", $image_data), 20);
+
+	$db->begin_transaction();
+	try
+	{
+		$query = "INSERT INTO posts (user_id, private, source, info, rating, width, height, created, mime, hash)";
+		$query .= " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		$stmt = $db->prepare($query);
+		$stmt->bind_param("iissiiiiss", $uid, $private, $source, $info, $rating, $width, $height, time(), $mime, $hash);
+		if (!$stmt->execute())
+			throw new Exception("Couldn't add post");
+
+		$post_id = $db->insert_id;
+		$tag_id = 0;
+
+		$stmt = $db->prepare("INSERT INTO post_tags (post_id, tag_id) VALUES (?, ?)");
+		$stmt->bind_param("ii", $post_id, $tag_id);
+		foreach ($tags as $tag)
+		{
+			$tag_id = get_tag_id($tag);
+			if (!$stmt->execute())
+				throw new Exception("Couldn't add post_tag");
+		}
+
+		$db->commit();
+	}
+	catch (Exception $ex)
+	{
+		$db->rollback();
+	}
+
+	//Move file to folder
+	//Create thumbnail
+}
+
+?>
