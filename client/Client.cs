6c636e1cb06d9e4a0564bb4b17825fdfbc62a402
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-18 22:38:15 +0100

    Implemented simple C# client

---
+++ client/Client.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace TA.Booru.Client
+{
+	public static class Client
+	{
+		public static void Main()
+		{
+			Console.Write("API URL: ");
+			string api_url = Console.ReadLine();
+
+			Console.Write("Username: ");
+			string username = Console.ReadLine();
+
+			Console.Write("Password: ");
+			string password = Console.ReadLine();
+
+			Console.Write("Image file: ");
+			byte[] image = File.ReadAllBytes(Console.ReadLine());
+
+			Console.Write("Source: ");
+			string source = Console.ReadLine();
+
+			Console.Write("Info: ");
+			string info = Console.ReadLine();
+
+			Console.Write("Rating: ");
+			byte rating = Convert.ToByte(Console.ReadLine());
+
+			Console.Write("Tags: ");
+			string[] tags = Console.ReadLine().Split(' ');
+
+			StringBuilder sb = new StringBuilder();
+			using (XMLFactory factory = new XMLFactory(sb, username, password))
+				factory.WriteUpload(image, false, source, info, rating, tags);
+			string request = sb.ToString();
+
+			Console.WriteLine();
+			Console.WriteLine(request);
+
+			API api = new API(api_url);
+			string response = api.RequestString(request);
+
+			Console.WriteLine();
+			Console.WriteLine(response);
+		}
+	}
+}
---
+++ client/XMLFactory.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Text;
+
+namespace TA.Booru.Client
+{
+	public class XMLFactory : IDisposable
+	{
+		private StringBuilder _SB;
+
+		public XMLFactory(StringBuilder SB, string Username, string Password)
+		{
+			_SB = SB;
+			_SB.AppendLine("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+			_SB.AppendLine("<Request>");
+			_SB.AppendLine("\t<Login>");
+			_SB.AppendLine("\t\t<Username>" + Username + "</Username>");
+			_SB.AppendLine("\t\t<Password>" + Password + "</Password>");
+			_SB.AppendLine("\t</Login>");
+		}
+
+		public void Dispose() { _SB.Append("</Request>"); }
+
+		public void WriteDelete(uint ID)
+		{
+			_SB.AppendLine("\t<Type>Delete</Type>");
+			_SB.AppendLine("\t<ID>" + ID + "</ID>");
+		}
+
+		public void WriteUpload(byte[] Image, bool Private, string Source, string Info, byte Rating, string[] Tags)
+		{
+			_SB.AppendLine("\t<Type>Upload</Type>");
+			_SB.AppendLine("\t<Image>" + Convert.ToBase64String(Image) + "</Image>");
+			_SB.AppendLine("\t<Post>");
+			_SB.AppendLine("\t\t<Private>" + (Private ? "1" : "0") + "</Private>");
+			_SB.AppendLine("\t\t<Source>" + Source + "</Source>");
+			_SB.AppendLine("\t\t<Info>" + Info + "</Info>");
+			_SB.AppendLine("\t\t<Rating>" + Rating + "</Rating>");
+			_SB.AppendLine("\t\t<Tags>");
+			foreach (string tag in Tags)
+				_SB.AppendLine("\t\t\t<Tag>" + tag + "</Tag>");
+			_SB.AppendLine("\t\t</Tags>");
+			_SB.AppendLine("\t</Post>");
+		}
+	}
+}
