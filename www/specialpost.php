0fe67ddc9002f7189e4f95479fee0752dbfca188
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-24 17:33:20 +0100

    BugFix: specialpost.php now checks private settings

--- www/specialpost.php
+++ www/specialpost.php
@@ -1,13 +1,21 @@
 <?php
 
 require_once("db.php");
+require_once("session.php");
 
-$query = "SELECT id FROM posts ORDER BY RAND() LIMIT 1";
+$query = "SELECT id FROM posts WHERE ";
+if (!session_loggedin())
+	$query .= "private = 0";
+else if (session_has_perm("admin"))
+	$query .= "1";
+else $query .= "(private = 0 OR user_id = " . session_user_id() . ")";
+
+$query_order_by = "RAND()";
 if (isset($_GET["type"]))
 	if ($_GET["type"] == "newest")
-		$query = "SELECT id FROM posts ORDER BY created DESC LIMIT 1";
+		$query = "created DESC";
 
-$result = $db->query($query);
+$result = $db->query($query . " ORDER BY " . $query_order_by . " LIMIT 1");
 $id = $result->fetch_row()[0];
 
 header("Location: post.php?id=" . $id);
