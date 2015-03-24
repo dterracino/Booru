094070b669363a06889e7e46fc48aaec42c9b90a
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-24 17:35:51 +0100

    Optimized query creation

--- www/specialpost.php
+++ www/specialpost.php
@@ -3,19 +3,18 @@
 require_once("db.php");
 require_once("session.php");
 
-$query = "SELECT id FROM posts WHERE ";
 if (!session_loggedin())
-	$query .= "private = 0";
+	$query_where = "private = 0";
 else if (session_has_perm("admin"))
-	$query .= "1";
-else $query .= "(private = 0 OR user_id = " . session_user_id() . ")";
+	$query_where = "1";
+else $query_where = "(private = 0 OR user_id = " . session_user_id() . ")";
 
 $query_order_by = "RAND()";
 if (isset($_GET["type"]))
 	if ($_GET["type"] == "newest")
 		$query = "created DESC";
 
-$result = $db->query($query . " ORDER BY " . $query_order_by . " LIMIT 1");
+$result = $db->query("SELECT id FROM posts WHERE " . $query_where . " ORDER BY " . $query_order_by . " LIMIT 1");
 $id = $result->fetch_row()[0];
 
 header("Location: post.php?id=" . $id);
