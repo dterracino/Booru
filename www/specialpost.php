5856e1233e18c9e27f00ef70ae80d61b3543bf34
teamalpha5441 <teamalpha5441@gmail.com>
2015-03-26 17:28:50 +0100

    BugFix: Fixed newest specialpost returning random post

--- www/specialpost.php
+++ www/specialpost.php
@@ -12,7 +12,7 @@ else $query_where = "(private = 0 OR user_id = " . session_user_id() . ")";
 $query_order_by = "RAND()";
 if (isset($_GET["type"]))
 	if ($_GET["type"] == "newest")
-		$query = "created DESC";
+		$query_order_by = "created DESC";
 
 $result = $db->query("SELECT id FROM posts WHERE " . $query_where . " ORDER BY " . $query_order_by . " LIMIT 1");
 $id = $result->fetch_row()[0];
